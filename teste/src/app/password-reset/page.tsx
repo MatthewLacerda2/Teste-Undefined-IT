4e92f542f4eb428363919c2cace12d3b87e5f7b1
Matheus Lacerda <matheus.l1996@gmail.com>
2024-09-02 17:18:15 -0300

    password reset page

--- teste/src/app/password-reset/page.tsx
+++ teste/src/app/password-reset/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import Button from "@/components/Button";
+import InputField from "@/components/InputField";
+import Logo from "@/components/Logo";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import Button from "../../components/Button";
-import InputField from "../../components/InputField";
 
 export default function PasswordResetPage() {
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
   const router = useRouter();
 
   const validatePassword = (value: string) => {
@@ -17,43 +20,78 @@ export default function PasswordResetPage() {
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setPassword(value);
+    setPassword(e.target.value);
+    setPasswordError(""); // Clear error when user starts typing
+  };
 
-    if (value === "") {
-      setPasswordError("");
-    } else if (!validatePassword(value)) {
-      setPasswordError(
-        "Senha inválida. Verifique se a senha tem pelo menos 8 caracteres com letras maiúsculas, minúsculas, números e caracteres especiais, e tente novamente."
-      );
-    } else {
-      setPasswordError("");
-    }
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordError(""); // Clear error when user starts typing
   };
 
   const handleSubmit = () => {
-    if (validatePassword(password)) {
-      router.push("/login");
-    } else {
+    if (password === "" || confirmPassword === "") {
+      setPasswordError("Por favor, preencha todos os campos.");
+      setConfirmPasswordError("Por favor, preencha todos os campos.");
+    } else if (
+      !validatePassword(password) ||
+      !validatePassword(confirmPassword)
+    ) {
       setPasswordError(
         "Senha inválida. Verifique se a senha tem pelo menos 8 caracteres com letras maiúsculas, minúsculas, números e caracteres especiais, e tente novamente."
       );
+      setConfirmPasswordError(
+        "Senha inválida. Verifique se a senha tem pelo menos 8 caracteres com letras maiúsculas, minúsculas, números e caracteres especiais, e tente novamente."
+      );
+    } else if (password !== confirmPassword) {
+      setConfirmPasswordError("As senhas não coincidem.");
+    } else {
+      alert("Senha alterada. Faça login.");
+      router.push("/reset-link");
     }
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
-      <div className="bg-white shadow-md rounded-lg w-full max-w-md p-6">
-        <h2 className="text-xl font-bold mb-4">Criar nova senha</h2>
-        <InputField
-          label="Nova Senha"
-          type="password"
-          placeholder="********"
-          value={password}
-          onChange={handlePasswordChange}
-          error={passwordError}
-        />
-        <Button text="Confirmar" onClick={handleSubmit} />
+    <div className="min-h-screen flex flex-col justify-center items-center bg-white p-4">
+      <Logo />
+      <div className="w-full max-w-md mt-8">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <h2 className="text-xl font-bold text-center mb-4">
+            Redefinir senha
+          </h2>
+          <p className="text-gray-600 text-center mb-6">
+            Redefina sua senha com no mínimo 6 caracteres
+          </p>
+          <InputField
+            label="Senha"
+            type="password"
+            placeholder="Digite uma senha"
+            value={password}
+            onChange={handlePasswordChange}
+            error={passwordError}
+          />
+          <InputField
+            label="Confirme sua senha"
+            type="password"
+            placeholder="Repita sua senha"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            error={confirmPasswordError}
+          />
+          <div className="text-sm font-bold text-gray-600 mt-4">
+            <p>Crie uma senha segura</p>
+            <ul className="font-normal list-disc ml-4">
+              <li>use letras maiúsculas e minúsculas, símbolos e números;</li>
+              <li>não use informações pessoais como datas de aniversário;</li>
+              <li>não use uma senha igual à anterior.</li>
+            </ul>
+          </div>
+          <div className="mt-8">
+            <Button text="Redefinir senha" onClick={handleSubmit} />
+          </div>
+        </div>
       </div>
     </div>
   );
